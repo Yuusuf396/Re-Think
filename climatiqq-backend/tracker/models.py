5d0b667bfd650017116f61fda026e5c00c43038c
Yuusuf Adebayo <adebayoayomide396@gmail.com>
2025-06-03 23:37:24 -0300

    setting up

--- climatiqq-backend/tracker/models.py
+++ climatiqq-backend/tracker/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ImpactLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    metric_type = models.CharField(max_length=20)  # carbon, water, energy
+    value = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
